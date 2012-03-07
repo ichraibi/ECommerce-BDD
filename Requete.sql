0fae0ad73ccfef55e2d501198b9e328c139b331d
Ikram Chraibi <ichraibi@epsi.fr>
2012-03-07 14:32:15 +0100

    requete sql creer

---
+++ Requete.sql
@@ -0,0 +1,40 @@
+--consultation du catalogue-----------------
+
+
+
+
+--sélection d'un ou plusieurs produits, en un ou plusieurs exemplaires, dans un "panier électronique"---------
+
+
+
+
+
+
+
+--passage en caisse avec vidage du panier et calcul du montant à régler---------------------
+
+
+
+
+
+
+
+
+-- gestion des coordonnées bancaires et postales des clients--------------
+
+
+
+
+
+-- gestion et authentification des connexions sur le site------------------
+
+
+
+
+
+
+
+-- option de présentation de l'historique des commandes du client-----------
+
+
+
